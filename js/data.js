b942883b339dbf1ff96fd22ebad7ea483794d1f3
ItsKristian01 <kristianhana11@gmail.com>
2023-11-07 15:51:47 +0100

    added carousel exercise code with arrows change

--- js/data.js
+++ js/data.js
@@ -22,3 +22,49 @@ const images = [
     }
 ];
 
+
+
+const itemsElem = document.querySelector(".items");
+console.log(itemsElem);
+
+
+let imagesString = "";
+for (let i = 0; i < images.length; i++) {
+  const curElem = images[i];
+  imagesString += `
+    <div class="item">
+         <img src="./${curElem.image}" alt="">
+    </div>`;
+}
+
+console.log(imagesString);
+
+itemsElem.innerHTML = itemsElem.innerHTML + imagesString;
+
+
+let currentIndex = 0; 
+const slideElems = document.querySelectorAll(".item");
+slideElems[currentIndex].classList.add("active");
+
+
+document.querySelector(".next").addEventListener("click", function () {
+
+  slideElems[currentIndex].classList.remove("active");
+
+  if (currentIndex < slideElems.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  slideElems[currentIndex].classList.add("active");
+});
+
+document.querySelector(".prev").addEventListener("click", function() {
+    slideElems[currentIndex].classList.remove("active");
+    if(currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = slideElems.length - 1;
+    }
+    slideElems[currentIndex].classList.add("active");
+})
